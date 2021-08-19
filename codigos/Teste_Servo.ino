e2b4aa28edf272eb3a582f41c02a5c25db3ffcb1
Laura Martin Werneck <84626462+LauraMWerneck@users.noreply.github.com>
2021-08-19 19:10:58 -0300

    Update Teste_Servo.ino

--- codigos/Teste_Servo.ino
+++ codigos/Teste_Servo.ino
@@ -1,24 +1,26 @@
 
-int Pinbuzzer = 2;
-int PinA0 = A0;
-
-//int leitura_sensor = 300;
-
-void setup() {
-  pinMode(PinA0, INPUT);
-  pinMode(Pinbuzzer, OUTPUT);
-  Serial.begin(9600);
-}
-void loop() {
-  int valor_analogico = analogRead(PinA0);
-  Serial.print("Leitura: ");
-  Serial.println(valor_analogico);
-  if (valor_analogico > 300) {
-    digitalWrite(Pinbuzzer, LOW);
-  }
-  else{
-    noTone(Pinbuzzer);
-    digitalWrite(Pinbuzzer, HIGH);
-  }
-  delay(1000);
-}
+#include <Servo.h> 
+ 
+Servo myservo; 
+ 
+int pos = 0; 
+ 
+void setup() 
+{ 
+  myservo.attach(8); 
+} 
+  
+void loop() 
+{ 
+  for(pos = 0; pos < 180; pos += 1) 
+  {                                   
+    myservo.write(pos); 
+    delay(6); 
+  } 
+  
+  for(pos = 180; pos>=1; pos-=1) 
+  {                                
+    myservo.write(pos); 
+    delay(6); 
+  }  
+} 
