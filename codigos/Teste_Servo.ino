49a353eeed07cc388e61cef402435979cd77b42b
Laura Martin Werneck <84626462+LauraMWerneck@users.noreply.github.com>
2021-08-26 18:17:57 -0300

    Update Teste_Servo.ino

--- codigos/Teste_Servo.ino
+++ codigos/Teste_Servo.ino
@@ -1,4 +1,10 @@
-
+/**********************************************************************************************************
+  Instituto Federal de Educação, Ciência e Tecnologia de Santa Catarina-Campus Florianópolis
+  Unidade Curricular: Projeto Integrador II
+  Aluna: Laura Martin Werneck
+  Semestre 2021.1
+  Objetivo: Teste do funcionamento do servo motor.
+************************************************************************************************************/
 #include <Servo.h> 
  
 Servo myservo; 
