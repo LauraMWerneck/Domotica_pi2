24044f8a8015345e9c769377b1705bf714af5cab
Laura Martin Werneck <84626462+LauraMWerneck@users.noreply.github.com>
2021-08-26 17:43:36 -0300

    Update Projeto_dom_tica.ino

--- codigos/Projeto_dom_tica.ino
+++ codigos/Projeto_dom_tica.ino
@@ -1,38 +1,37 @@
-
-#define LED_Q1 13 // Q1 = quarto 1 (perto da cozinha)
-#define LED_Q2 12 // Q2 = quarto 2 (perto da sala)
-#define LED_S 11 // S = sala
-#define LED_J 10 // J = jardim
+#define LED_Q1 13 /* Q1 = quarto 1 (perto da cozinha) */
+#define LED_Q2 12 /* Q2 = quarto 2 (perto da sala) */
+#define LED_S 11 /* S = sala */
+#define LED_J 10 /* J = jardim */
 #define PIR 9
 
 #define Buzzer 2
 #define MQ_2 A0
-#include <Servo.h> //incluir a biblioteca de servo
-Servo myservo;  //Inclusao do servo
-int pos = 0;  //PosiÃƒÂ§ÃƒÂ£o zero para o servo
+#include <Servo.h> /* incluir a biblioteca de servo */
+Servo myservo;  /* Inclusão do servo */
+int pos = 0;  /* Posição zero para o servo */
 
 void setup() {
-  /* ComunicaÃƒÂ§ÃƒÂ£o serial com o computador */
+  /* Comunicação serial com o computador */
   Serial.begin(9600);
   while (!Serial);
-  /* ConfiguraÃƒÂ§ÃƒÂ£o dos pinos como entrada ou saÃƒÂ­da */
+  /* Configuração dos pinos como entrada ou saída */
   pinMode(LED_Q1, OUTPUT);
   pinMode(LED_Q2, OUTPUT);
   pinMode(LED_J, OUTPUT);
   pinMode(LED_S, OUTPUT);
   pinMode(PIR, INPUT);
 
-  myservo.attach(8);  //Pino do servo+arduino
-  myservo.write(0);  //Servo setado com valor zero
+  myservo.attach(8);  /*Pino do servo+arduino */
+  myservo.write(0);  /* Servo setado com valor zero */
 
   pinMode(MQ_2, INPUT);
  pinMode(Buzzer, OUTPUT);
 
   Serial.flush();  //Limpando a memoria do serial
-  Serial.println("Digite: \n 'a' para acender a luz do quarto 1 (prÃƒÂ³ximo ÃƒÂ  cozinha) \n 'A' para apagar a luz do quarto 1");
-  Serial.println(" 'b' para acender a luz do quarto 2 (prÃƒÂ³ximo ÃƒÂ  sala) \n 'B' para apagar a luz do quarto 2");
+  Serial.println("Digite: \n 'a' para acender a luz do quarto 1 (próximo à  cozinha) \n 'A' para apagar a luz do quarto 1");
+  Serial.println(" 'b' para acender a luz do quarto 2 (próximo à  sala) \n 'B' para apagar a luz do quarto 2");
   Serial.println(" 'c' para acender a luz da sala \n 'C' para apagar a luz da sala \n 'd' para apagar todas as luzes internas");
-  Serial.println(" 'e' para arir o portÃƒÂ£o \n 'E' para fechar o portÃƒÂ£o");
+  Serial.println(" 'e' para arir o portão \n 'E' para fechar o portão");
 }
 
 
@@ -102,6 +101,7 @@ void tarefa_3() {
   Serial.print("Leitura: ");
   Serial.println(valor_analogico);
   if (valor_analogico > 300) {
+    
     digitalWrite(Buzzer, LOW);
   }
   else {
