0abc4d21d98a646a0973773e6215b5586dcd8c04
Laura Martin Werneck <84626462+LauraMWerneck@users.noreply.github.com>
2021-09-01 20:45:56 -0300

    Update Teste_PIR_LEDs.ino

--- codigos/Teste_PIR_LEDs.ino
+++ codigos/Teste_PIR_LEDs.ino
@@ -5,31 +5,32 @@
   Semestre 2021.1
   Objetivo: Teste do sendor PIR com LEDs.
 ************************************************************************************************************/
-#define led 7
-#define led2 5
-#define pirSensor 9
+
+#define LED 7 // LED 1 conectado ao pino digital 7.
+#define LED2 5 // LED 2 conectado ao pino digital 5.
+#define PIR 9  // PIR conectado ao pino digital 9.
 
 void setup() { 
- Serial.begin(9600);
-  pinMode(pirSensor, INPUT); 
-  pinMode(led,OUTPUT);
-  pinMode(led2, OUTPUT);
+ Serial.begin(9600); // Inicia a porta serial, configura a taxa de dados para 9600 bps.
+  pinMode(PIR, INPUT); // Define o PIR como entrada.
+  pinMode(LED,OUTPUT); // Define o LED 1 como saida.
+  pinMode(LED2, OUTPUT); // Define o LED 2 como saida.
 }
 
 void loop() {
   
-int x= digitalRead(pirSensor);
+int x= digitalRead(PIR); // Lê o valor do pino do PIR.
 
-  if(x==LOW)
+  if(x==LOW) // Caso o sensor não capte movimento.
   {
-    digitalWrite(led,LOW);
-    digitalWrite(led2, HIGH);
+    digitalWrite(LED,LOW); // LED 1 continua apagado.
+    digitalWrite(LED2, HIGH); // Acende o LED 2.
     Serial.println(x);
   }
-  else
+  else // Caso o sensor capte movimento.
   {
-    digitalWrite(led,HIGH);
-    digitalWrite(led2, LOW);
-    Serial.println(x);
+    digitalWrite(LED,HIGH); // Acende o LED 1.
+    digitalWrite(LED2, LOW); // Apaga o LED 2.
+    Serial.println(x);//Imprime dados na porta serial
   }
 }
