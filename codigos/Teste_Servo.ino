25d74de610756db750e4a12ad8f8edb8037f0968
Laura Martin Werneck <84626462+LauraMWerneck@users.noreply.github.com>
2021-09-01 21:25:00 -0300

    Update Teste_Servo.ino

--- codigos/Teste_Servo.ino
+++ codigos/Teste_Servo.ino
@@ -5,28 +5,28 @@
   Semestre 2021.1
   Objetivo: Teste do funcionamento do servo motor.
 ************************************************************************************************************/
-#include <Servo.h> 
+#include <Servo.h> // Inclui a biblioteca do Servo.
  
 Servo myservo; 
  
-int pos = 0; 
+int pos = 0; // Posição Servo.
  
 void setup() 
 { 
-  myservo.attach(8); 
+  myservo.attach(8); // Anexa a variável Servo ao pino 8.
 } 
   
 void loop() 
 { 
-  for(pos = 0; pos < 180; pos += 1) 
+  for(pos = 0; pos < 180; pos += 1) // Aumenta o angulo do servo ate chegar em 180 graus
   {                                   
-    myservo.write(pos); 
+    myservo.write(pos); // Grava o valor da posição do servo e controla o eixo de acordo.
     delay(6); 
   } 
   
-  for(pos = 180; pos>=1; pos-=1) 
+  for(pos = 180; pos>=1; pos-=1)  // Diminui o angulo do servo
   {                                
     myservo.write(pos); 
-    delay(6); 
+    delay(6); //Pausa o programa por 6 ms.
   }  
 } 
